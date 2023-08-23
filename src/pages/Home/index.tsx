9c6bf26913a674a30aaf8be334b909d9bf7265b3
vsantos1711 <vsantos067100@gmail.com>
2023-08-23 17:40:08 -0300

    feat: add Card component on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import * as S from './style.ts'
 import {Input} from "../../components/Input";
 import {Header} from "../../components/Header";
+import {Card} from "../../components/Card";
 
 export const Home = () => {
   return (
     <S.Container>
       <Header/>
       <Input/>
+      <Card/>
     </S.Container>
   );
 };
