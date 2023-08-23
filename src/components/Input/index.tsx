20564e073b74df43b2635bdd36b1a5be6837dc39
vsantos1711 <vsantos067100@gmail.com>
2023-08-23 11:43:42 -0300

    chore: start Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './style.ts'
+import {SearchCode} from "lucide-react";
+
+export const Input = () => {
+  return (
+    <S.InputContainer>
+      <SearchCode color="#0079FE" size='3rem'/>
+      <S.Input type="text" name="dev-name" id="dev-name" placeholder="Search GitHub username..." />
+      <S.Button>Search</S.Button>
+    </S.InputContainer>
+  );
+};
