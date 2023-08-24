9956726b6453ae851d1937ab2acb424ae1b3d4bb
vsantos1711 <vsantos067100@gmail.com>
2023-08-24 17:20:06 -0300

    feat: create Card Props interface

---
+++ src/pages/Home/api/interface/index.ts
@@ -0,0 +1,15 @@
+export interface CardProps {
+  name: string;
+  created_at: string;
+  login: string;
+  url: string;
+  bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  htmlUrl: string;
+  blog: string;
+  twitter_username: string;
+  location: string;
+  avatar_url: string;
+}
