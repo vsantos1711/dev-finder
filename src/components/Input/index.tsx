68f4fdfa15454aa8274c485f27758bd00241002b
vsantos1711 <vsantos067100@gmail.com>
2023-08-24 09:26:00 -0300

    fix: remove unnecessary color property

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,7 @@ import {SearchCode} from "lucide-react";
 export const Input = () => {
   return (
     <S.InputContainer>
-      <SearchCode color="#0079FE" size='3rem'/>
+      <SearchCode size='3rem'/>
       <S.Input type="text" name="dev-name" id="dev-name" placeholder="Search GitHub username..." />
       <S.Button>Search</S.Button>
     </S.InputContainer>
