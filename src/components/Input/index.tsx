94adbe8e64c195d634bcbd6b8e8d87ec0ba2d9c8
vsantos1711 <vsantos067100@gmail.com>
2023-08-24 17:22:15 -0300

    feat: add Input props and events

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,17 @@
 import * as S from './style.ts'
 import {SearchCode} from "lucide-react";
 
-export const Input = () => {
+interface InputProps {
+  onClick: () => void,
+  setUsername: (username: string) => void
+}
+export const Input = ({ onClick, setUsername }: InputProps) => {
+
   return (
-    <S.InputContainer>
+    <S.InputContainer onSubmit={(e) => e.preventDefault()}>
       <SearchCode size='3rem'/>
-      <S.Input type="text" name="dev-name" id="dev-name" placeholder="Search GitHub username..." />
-      <S.Button>Search</S.Button>
+      <S.Input type="text" name="dev-name" id="dev-name" placeholder="Search GitHub username..." onChange={({target}) => setUsername(target.value)}/>
+      <S.Button onClick={onClick}>Search</S.Button>
     </S.InputContainer>
   );
 };
