880ef28ba4d88cbcca33391a2f5e867661cda586
vsantos1711 <vsantos067100@gmail.com>
2023-08-24 17:24:17 -0300

    chore: add useStates and Card props in the Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,33 @@
 import * as S from './style.ts'
-import {Input} from "../../components/Input";
-import {Header} from "../../components/Header";
-import {Card} from "../../components/Card";
+import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Card } from "../../components/Card";
+import { useState } from "react";
+import {
+  getData
+} from './api'
+import { CardProps } from "./api/interface";
 
 export const Home = () => {
+
+  const[data, setData] = useState<CardProps>()
+  const[username, setUsername] = useState('')
+  const[alert, setAlert] = useState('')
+
+  async function clickHandler() {
+    if(!username) setAlert('Please, enter a username')
+    const userInfos = await getData(username);
+    setData(userInfos);
+    setAlert('')
+  }
+
   return (
     <S.Container>
       <Header/>
-      <Input/>
-      <Card/>
+      <Input onClick={clickHandler} setUsername={setUsername}/>
+      {data && !alert && <Card {...data} />}
+      {alert && <h2>{alert}</h2>}
+
     </S.Container>
   );
 };
