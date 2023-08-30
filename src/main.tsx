f9d4ba670d4200f2898cd579e9351038ee3e6612
vsantos1711 <vsantos067100@gmail.com>
2023-08-30 11:44:09 -0300

    feat: add Alert Component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Alert } from './components/Alert'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Alert />
   </React.StrictMode>,
 )
