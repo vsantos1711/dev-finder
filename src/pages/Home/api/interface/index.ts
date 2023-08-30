b7c00bdc9defb6d0a6b492fc7d2578a1e4fefd0f
vsantos1711 <vsantos067100@gmail.com>
2023-08-30 11:39:58 -0300

    fix: change props names on Card Props interface

--- src/pages/Home/api/interface/index.ts
+++ src/pages/Home/api/interface/index.ts
@@ -2,14 +2,14 @@ export interface CardProps {
   name: string;
   created_at: string;
   login: string;
-  url: string;
   bio: string;
   public_repos: number;
   followers: number;
   following: number;
-  htmlUrl: string;
+  html_url: string;
   blog: string;
   twitter_username: string;
   location: string;
+  company: string;
   avatar_url: string;
 }
