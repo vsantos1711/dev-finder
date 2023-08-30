27378f293f02b884e7e5c1f362361b96fe7857ef
vsantos1711 <vsantos067100@gmail.com>
2023-08-30 11:42:22 -0300

    feat: add theme change on Button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,30 @@
 import * as S from "./style.ts"
-import {Sun} from "lucide-react";
+import {Moon, Sun} from "lucide-react";
+import {AppContext} from "../../context";
+import {useContext} from "react";
+import {darkTheme, lightTheme} from "../../style/theme.ts";
 
 export const Header = () => {
+
+  const{theme, setTheme} = useContext(AppContext)
+  function changeTheme(){
+    theme === darkTheme ? setTheme(lightTheme) : setTheme(darkTheme)
+  }
+
   return (
     <S.Header>
       <S.Title>devfinder</S.Title>
-      <S.Theme>LIGHT <Sun/></S.Theme>
+      <S.Button onClick={changeTheme}>
+        {theme === darkTheme ? (
+          <>
+            LIGHT <Sun />
+          </>
+        ) : (
+          <>
+            DARK <Moon />
+          </>
+        )}
+      </S.Button>
     </S.Header>
   );
 };
