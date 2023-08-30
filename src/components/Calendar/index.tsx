c3bbbcbb01834e6e2988ec065f6dd039f4988716
vsantos1711 <vsantos067100@gmail.com>
2023-08-29 22:36:40 -0300

    feat: create Calendar component and theme

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,16 +1,23 @@
 import GitHubCalendar from "react-github-calendar";
-//import ColorScale from "react-activity-calendar/color-scale";
 
 interface CalendarProps {
   username: string;
 }
-//const gitHubTheme = ["#505050", "#007E51", "#009B63", "#00B876", "#00D98B"];
+
+const themeDark = ["#505050", "#007E51", "#009B63", "#00B876", "#00D98B"]
+//const themeLight = ["#EBEDF0", "#9BE9A8", "#40C463", "#30A14E", "#216E39"]
 
 export const Calendar = ({ username }: CalendarProps) => {
+
+
   return (
+    <>
     <GitHubCalendar
       username={username}
-      theme={{ dark: ["#505050", "#007E51", "#009B63", "#00B876", "#00D98B"] }}
+      theme={{
+        dark: [themeDark[0], themeDark[1], themeDark[2], themeDark[3], themeDark[4]],
+      }}
     />
+    </>
   );
 };
