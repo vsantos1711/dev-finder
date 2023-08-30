fc612f50c9e08da5bd7356f4f77cc3f137a8d456
vsantos1711 <vsantos067100@gmail.com>
2023-08-29 22:45:17 -0300

    feat: add Calendar component on Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,80 +1,79 @@
 import * as S from './style.ts';
-import {Building2, Link, MapPin, Twitter} from "lucide-react";
-import {CardProps} from "../../pages/Home/api/interface";
-import {Calendar} from "../Calendar";
+import { Building2, Link, MapPin, Twitter } from "lucide-react";
+import { CardProps } from "../../pages/Home/api/interface";
+import { Calendar } from "../Calendar";
 
 export const Card = ({name, created_at, login, url, bio, public_repos, followers, following, location, htmlUrl, twitter_username, blog, avatar_url}: CardProps) => {
   return (
     <>
-    <S.CardContainer>
-      <S.TitleContainer>
-        <S.Image>
-          <img alt="profile photo" src={avatar_url}/>
-        </S.Image>
+      <S.CardContainer>
+        <S.TitleContainer>
+          <S.Image>
+            <img alt="profile photo" src={avatar_url}/>
+          </S.Image>
 
-        <S.TitleContent>
-          <S.Title>
+          <S.TitleContent>
+            <S.Title>
 
-            <S.H2>
-              {name}
-            </S.H2>
+              <S.H2>
+                {name}
+              </S.H2>
 
-            <S.Span>Joined at {new Date(created_at).toLocaleDateString('en-US',{
-              day: '2-digit',
-              month: 'long',
-              year: 'numeric'
-            })}</S.Span>
-          </S.Title>
+              <S.Span>Joined at {new Date(created_at).toLocaleDateString('en-US',{
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+              })}</S.Span>
+            </S.Title>
 
-          <S.Link href={url}>
-            @{login}
-          </S.Link>
-        </S.TitleContent>
-      </S.TitleContainer>
+            <S.Link href={url}>
+              @{login}
+            </S.Link>
+          </S.TitleContent>
+        </S.TitleContainer>
 
-      <S.ContentContainer>
-        <S.Paragraph>
-          {bio}
-        </S.Paragraph>
+        <S.ContentContainer>
+          <S.Paragraph>
+            {bio}
+          </S.Paragraph>
 
-        <S.UserContent>
+          <S.UserContent>
+            <S.UserItem>
+              <S.UserItemTitle>Repos</S.UserItemTitle>
+              <S.UserItemData>{public_repos}</S.UserItemData>
+            </S.UserItem>
 
-          <S.UserItem>
-            <S.UserItemTitle>Repos</S.UserItemTitle>
-            <S.UserItemData>{public_repos}</S.UserItemData>
-          </S.UserItem>
+            <S.UserItem>
+              <S.UserItemTitle>Followers</S.UserItemTitle>
+              <S.UserItemData>{followers}</S.UserItemData>
+            </S.UserItem>
 
-          <S.UserItem>
-            <S.UserItemTitle>Followers</S.UserItemTitle>
-            <S.UserItemData>{followers}</S.UserItemData>
-          </S.UserItem>
+            <S.UserItem>
+              <S.UserItemTitle>Following</S.UserItemTitle>
+              <S.UserItemData>{following}</S.UserItemData>
+            </S.UserItem>
+          </S.UserContent>
 
-          <S.UserItem>
-            <S.UserItemTitle>Following</S.UserItemTitle>
-            <S.UserItemData>{following}</S.UserItemData>
-          </S.UserItem>
-        </S.UserContent>
-
-        <S.UserLinksContent>
-          <S.UserLink href='#'>
-            <MapPin/>
-            {location ?? 'Not available' }
-          </S.UserLink>
-          <S.UserLink href='#'>
-            <Twitter />
-            {twitter_username ?? 'Not available' }
-          </S.UserLink>
-          <S.UserLink href='#'>
-            <Link />
-            {blog ?? 'Not available' }
-          </S.UserLink>
-          <S.UserLink href='#'>
-            <Building2/>
-            {htmlUrl ?? 'Not available' }
-          </S.UserLink>
-        </S.UserLinksContent>
-      </S.ContentContainer>
-    </S.CardContainer>
+          <S.UserLinksContent>
+            <S.UserLink href='#'>
+              <MapPin/>
+              {location ?? 'Not available' }
+            </S.UserLink>
+            <S.UserLink href='#'>
+              <Twitter />
+              {twitter_username ?? 'Not available' }
+            </S.UserLink>
+            <S.UserLink href={blog} target="_blank">
+              <Link />
+              {blog ?? 'Not available' }
+            </S.UserLink>
+            <S.UserLink href='#' >
+              <Building2/>
+              {htmlUrl ?? 'Not available' }
+            </S.UserLink>
+          </S.UserLinksContent>
+        </S.ContentContainer>
+      </S.CardContainer>
       <S.CardContainer>
         <Calendar username={login}/>
       </S.CardContainer>
