9148e94c2175063aa9caed7c6780ed8470b0d033
vsantos1711 <vsantos067100@gmail.com>
2023-09-06 16:20:06 -0300

    chore: create routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Alert } from './components/Alert'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Alert } from "./components/Alert";
+import { AppContextProvider } from "./context";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { UserProfile } from "./pages/UserProfile/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:user",
+    element: <UserProfile />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <AppContextProvider>
+      <RouterProvider router={router} />
+    </AppContextProvider>
     <Alert />
-  </React.StrictMode>,
-)
+  </>
+);
