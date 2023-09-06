0df8f1e4e7e149744436effcfbe1c63177370940
vsantos1711 <vsantos067100@gmail.com>
2023-09-06 16:24:04 -0300

    remove unnecessary commentary

--- src/pages/Home/api/index.ts
+++ src/pages/Home/api/index.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.github.com/",
 });
-
-//repository
 export async function getData(githubUsername: string) {
   return (await api.get(`users/${githubUsername}`)).data;
 }
