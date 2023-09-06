a9721f613f654539a347ffa89ee2a5888c9c9bde
vsantos1711 <vsantos067100@gmail.com>
2023-09-06 16:20:32 -0300

    feat: add Link to Home on logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
-import * as S from "./style.ts"
-import {Moon, Sun} from "lucide-react";
-import {AppContext} from "../../context";
-import {useContext} from "react";
-import {darkTheme, lightTheme} from "../../style/theme.ts";
+import * as S from "./style.ts";
+import { Moon, Sun } from "lucide-react";
+import { AppContext } from "../../context";
+import { useContext } from "react";
+import { darkTheme, lightTheme } from "../../style/theme.ts";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-
-  const{theme, setTheme} = useContext(AppContext)
-  function changeTheme(){
-    theme === darkTheme ? setTheme(lightTheme) : setTheme(darkTheme)
+  const { theme, setTheme } = useContext(AppContext);
+  function changeTheme() {
+    theme === darkTheme ? setTheme(lightTheme) : setTheme(darkTheme);
   }
 
   return (
     <S.Header>
-      <S.Title>devfinder</S.Title>
+      <Link to="/">
+        <S.Title>devfinder</S.Title>
+      </Link>
       <S.Button onClick={changeTheme}>
         {theme === darkTheme ? (
           <>
